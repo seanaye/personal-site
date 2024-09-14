975ce8fa5e69bd6dc72e2944aef557f933537497
Sean Aye <sean.aye@agilebits.com>
2024-09-14 11:52:57 -0400

    cargo fmt

--- server/src/bucket.rs
+++ server/src/bucket.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
-use itertools::Itertools;
-use url::Url;
 use futures::{stream, Stream, StreamExt};
-use s3::{bucket::Bucket, bucket_ops::ListBucketsResponse, creds::Credentials, error::S3Error, serde_types::ListBucketResult, Region};
+use itertools::Itertools;
+use s3::{
+    bucket::Bucket, bucket_ops::ListBucketsResponse, creds::Credentials, error::S3Error,
+    serde_types::ListBucketResult, Region,
+};
+use url::Url;
 
 pub fn get_bucket() -> Result<Box<Bucket>, S3Error> {
-
     let key = std::env::var("R2_ACCESS_KEY").unwrap();
     let secret = std::env::var("R2_SECRET_KEY").unwrap();
     let credentials = Credentials {
@@ -22,18 +24,17 @@ pub fn get_bucket() -> Result<Box<Bucket>, S3Error> {
             account_id: std::env::var("R2_ACCOUNT_ID").unwrap(),
         },
         credentials,
-    ).map(|mut bucket| {
-            bucket.set_listobjects_v2();
-            bucket
-        })
+    )
+    .map(|mut bucket| {
+        bucket.set_listobjects_v2();
+        bucket
+    })
 }
 
 pub struct BucketAccess<'a> {
     bucket: Box<Bucket>,
-    host: &'a str
+    host: &'a str,
 }
-
-
 
 #[derive(Debug)]
 pub struct ResizedImage {
@@ -50,46 +51,55 @@ impl<'a> BucketAccess<'a> {
     async fn list_recursive(&self, prefix: String) -> anyhow::Result<Vec<ListBucketResult>> {
         let mut res = self.bucket.list(prefix, Some("/".into())).await?;
 
-        let common: Vec<_> = res.iter_mut().flat_map(|r| r.common_prefixes.iter_mut().flat_map(|v| v.iter_mut()).map(|v| std::mem::take(&mut v.prefix))).collect();
+        let common: Vec<_> = res
+            .iter_mut()
+            .flat_map(|r| {
+                r.common_prefixes
+                    .iter_mut()
+                    .flat_map(|v| v.iter_mut())
+                    .map(|v| std::mem::take(&mut v.prefix))
+            })
+            .collect();
         for prefix in common {
             let next = self.list_recursive(prefix).await?;
             res.extend(next);
         }
 
         Ok(res)
-        
     }
 
     pub async fn list_resized(&self) -> anyhow::Result<HashMap<String, Vec<ResizedImage>>> {
         let res = self.list_recursive("resized/".into()).await?;
-        let mut objects: Vec<_> = res.into_iter().flat_map(|c| c.contents.into_iter()).filter_map(|c| Some((c.key.split("/").last()?.to_string(), c))).collect();
+        let mut objects: Vec<_> = res
+            .into_iter()
+            .flat_map(|c| c.contents.into_iter())
+            .filter_map(|c| Some((c.key.split("/").last()?.to_string(), c)))
+            .collect();
         objects.sort_unstable_by_key(|(key, _)| key.clone());
         let mut out = HashMap::new();
         for (key, val) in &objects.into_iter().chunk_by(|(key, _)| key.clone()) {
-            let resized_images: Vec<_> = stream::iter(val).filter_map(|(_key, c)| async move {
-                let mut host = self.bucket.host();
-                host.push('/');
-                host.push_str(&c.key);
-                host.replace_range(0..0, "https://");
-                let mut url: Url = host.parse().ok()?;
-                url.set_host(Some(self.host));
-                let (head, _status) = self.bucket.head_object(c.key).await.ok()?;
-                let metadata = &mut head.metadata?;
-                let aspect_ratio = metadata.get_mut("aspect-ratio")?;
-                Some(ResizedImage {
-                    url,
-                    aspect_ratio: std::mem::take(aspect_ratio)
+            let resized_images: Vec<_> = stream::iter(val)
+                .filter_map(|(_key, c)| async move {
+                    let mut host = self.bucket.host();
+                    host.push('/');
+                    host.push_str(&c.key);
+                    host.replace_range(0..0, "https://");
+                    let mut url: Url = host.parse().ok()?;
+                    url.set_host(Some(self.host));
+                    let (head, _status) = self.bucket.head_object(c.key).await.ok()?;
+                    let metadata = &mut head.metadata?;
+                    let aspect_ratio = metadata.get_mut("aspect-ratio")?;
+                    Some(ResizedImage {
+                        url,
+                        aspect_ratio: std::mem::take(aspect_ratio),
+                    })
                 })
-            }).collect().await;
+                .collect()
+                .await;
 
             out.insert(key.to_string(), resized_images);
-
-            
         }
 
         Ok(out)
-        
     }
 }
-
-
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, sync::Arc};
 use app::*;
 use axum::Router;
 use bucket::{get_bucket, BucketAccess};
 use grid::{Dimension, FromAspectRatio, RoundedAspectRatio};
-use photogrid::{PhotoLayoutData, ResponsivePhotoGrid, SrcSet};
 use leptos::{attr::Srcset, prelude::*};
 use leptos_axum::{generate_route_list, LeptosRoutes};
+use photogrid::{PhotoLayoutData, ResponsivePhotoGrid, SrcSet};
+use std::{collections::HashMap, sync::Arc};
 
 mod bucket;
 
@@ -14,18 +14,32 @@ async fn build_photo_grid() -> anyhow::Result<ResponsivePhotoGrid<PhotoLayoutDat
 
     let data = bucket.list_resized().await?;
 
-    let photo_data: Vec<_> = data.into_iter().filter_map(|(key, value)| {
-        let aspect_ratio = value.first()?.aspect_ratio.parse().ok()?;
-        Some(PhotoLayoutData {
-            aspect_ratio,
-            srcs: value.into_iter().map(|c| SrcSet { dimensions: Dimension { width: aspect_ratio.width, height: aspect_ratio.height } , url: c.url }).collect(),
-            metadata: HashMap::new(),
+    let photo_data: Vec<_> = data
+        .into_iter()
+        .filter_map(|(key, value)| {
+            let aspect_ratio = value.first()?.aspect_ratio.parse().ok()?;
+            Some(PhotoLayoutData {
+                aspect_ratio,
+                srcs: value
+                    .into_iter()
+                    .map(|c| SrcSet {
+                        dimensions: Dimension {
+                            width: aspect_ratio.width,
+                            height: aspect_ratio.height,
+                        },
+                        url: c.url,
+                    })
+                    .collect(),
+                metadata: HashMap::new(),
+            })
         })
-    }).collect();
+        .collect();
 
-    Ok(ResponsivePhotoGrid::new(photo_data, [3, 4, 5, 8, 12], |x| {
-        RoundedAspectRatio::<2>::from_aspect_ratio(&x.aspect_ratio)
-    }))
+    Ok(ResponsivePhotoGrid::new(
+        photo_data,
+        [3, 4, 5, 8, 12],
+        |x| RoundedAspectRatio::<2>::from_aspect_ratio(&x.aspect_ratio),
+    ))
 }
 
 #[tokio::main]
@@ -38,7 +52,12 @@ async fn main() {
     // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
     // Alternately a file can be specified such as Some("Cargo.toml")
     // The file would need to be included with the executable when moved to deployment
-    let conf = get_configuration(if cfg!(debug_assertions) { None } else { Some("Cargo.toml")}).unwrap();
+    let conf = get_configuration(if cfg!(debug_assertions) {
+        None
+    } else {
+        Some("Cargo.toml")
+    })
+    .unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
@@ -46,10 +65,15 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        .leptos_routes_with_context(&leptos_options, routes, move || provide_context(grid.clone()), {
-            let leptos_options = leptos_options.clone();
-            move || shell(leptos_options.clone())
-        })
+        .leptos_routes_with_context(
+            &leptos_options,
+            routes,
+            move || provide_context(grid.clone()),
+            {
+                let leptos_options = leptos_options.clone();
+                move || shell(leptos_options.clone())
+            },
+        )
         .fallback(leptos_axum::file_and_error_handler(shell))
         .with_state(leptos_options);
 
